673e90ead932288d39041e267063f68edfc63b38
tanvikohale <tanvikohale23@gmail.com>
2025-10-28 21:53:11 +0530

    Add project

---
+++ server/models/userSchema.js
@@ -0,0 +1,71 @@
+
+import { verify } from "jsonwebtoken";
+import mongoose from "mongoose";
+import bcrypt from "bcrypt"
+
+let addressObject = {
+    street: "", city: "", state: "", country: "", pincode: ""
+}
+
+let emailObject = {
+    email: "", verify: false
+}
+
+let userShcema = mongoose.Schema({
+    name: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: Object,
+        require: true,
+        default: emailObject
+    },
+    password: {
+        type: String,
+        require: true
+    },
+    phone: {
+        type: String,
+        require: true
+    },
+    address: {
+        type: Object,
+        require: true,
+        default: addressObject
+    },
+    dob: {
+        type: String,
+        require: true
+    },
+    qualifications: {
+        type: Array,
+        default: []
+    },
+    documents: {
+        type: Array,
+        default: []
+    },
+    appliedJobs: {
+        type: Array,
+        default: []
+    },
+    timeStamp: {
+        type: Date,
+        default: Date.now()
+    }
+})
+
+userShcema.pre("save", async function () {
+    try {
+        this.password = await bcrypt.hash(this.password, 10)
+        console.log("password hased and saved !")
+    } catch (err) {
+        console.log("error in pre method : ", err)
+        throw err
+    }
+})
+
+let userModel = new mongoose.model("users", userShcema)
+
+export { userModel }
